2b7c92b96265bf359eaaeb8e39b94153d2c0d1f9
Anthony Lukach <anthonylukach@gmail.com>
2023-06-21 17:02:37 -0700

    Update copy

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,10 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 
 export const metadata = {
-  title: 'Vercel Postgres Demo with Prisma',
+  title: "Técnico",
   description:
-    'A simple Next.js app with Vercel Postgres as the database and Prisma as the ORM',
-}
+    "A simple Next.js app with Vercel Postgres as the database and Prisma as the ORM",
+};
 
 const inter = Inter({
   variable: '--font-inter',
