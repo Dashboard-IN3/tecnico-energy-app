01aceaa4db6867c3f7b56b8be19668922b214775
Anthony Lukach <anthonylukach@gmail.com>
2024-02-06 15:40:10 -0800

    Mv data fetching to components

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,17 @@
-import { Header } from "../components/header"
-import CardGrid from "../components/card-grid"
+import CardGrid from "@/components/card-grid"
+import { Header } from "@/components/header"
 import { Study } from "@prisma/client"
-import { getStudies } from "./lib/data"
 
 export const revalidate = 3600 // revalidate the data at most every hour
 
-export default async function Home({}: Props) {
-  const studies = await getStudies()
+export default async function Home() {
   return (
     <div className="bg-slate-100 h-full flex flex-col overflow-x-hidden">
       <Header />
       <div className="container mx-2 px-2 sm:px-6 lg:px-8 py-8">
         <h1 className="text-xl font-bold">Explore Urban Data Studies</h1>
-        <CardGrid studies={studies} />
+        <CardGrid />
       </div>
     </div>
   )
-}
-
-interface Props {
-  studies: Study[]
 }
