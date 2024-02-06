49fe6691855fb5809a510e1e6deb244860a5fd35
Anthony Lukach <anthonylukach@gmail.com>
2024-02-05 18:39:19 -0800

    Add DB seeding logic

---
+++ prisma/migrations/20240206023826_initial/migration.sql
@@ -0,0 +1,47 @@
+-- CreateExtension
+CREATE EXTENSION IF NOT EXISTS "postgis";
+
+-- CreateTable
+CREATE TABLE "buildings" (
+    "name" TEXT NOT NULL,
+    "geom" geometry(Polygon, 4326) NOT NULL,
+    "properties" JSONB NOT NULL,
+
+    CONSTRAINT "buildings_pkey" PRIMARY KEY ("name")
+);
+
+-- CreateTable
+CREATE TABLE "studies" (
+    "id" SERIAL NOT NULL,
+    "slug" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "image_src" TEXT NOT NULL,
+
+    CONSTRAINT "studies_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "themes" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+
+    CONSTRAINT "themes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "scenarios" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "scenarios_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "studies_slug_key" ON "studies"("slug");
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,33 @@
-import prisma from "../lib/prisma";
+import prisma from "../lib/prisma"
 
-async function main() {}
+async function main() {
+  const studies = [
+    "Portugal Municipal Energy",
+    "Lisbon Building Energy",
+    "FPO",
+  ].map((name, idx) => ({
+    id: idx + 1,
+    name,
+    slug: name.toLowerCase().replaceAll(" ", "-"),
+    title: `Study ${name}`,
+    description: name,
+    image_src: `https://fakeimg.pl/600x400?text=${name}`,
+  }))
+  for (const study of studies) {
+    await prisma.studies.upsert({
+      where: { id: study.id },
+      update: study,
+      create: study,
+    })
+  }
+}
 
 main()
   .then(async () => {
-    await prisma.$disconnect();
+    await prisma.$disconnect()
   })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+  .catch(async e => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
