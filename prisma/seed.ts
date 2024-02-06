17508d740236e16cd23053dd14cac59d765dc40d
Anthony Lukach <anthonylukach@gmail.com>
2024-02-06 09:17:48 -0800

    Start

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,22 +1,31 @@
+import { Prisma } from "@prisma/client"
 import prisma from "../lib/prisma"
+import yaml from "js-yaml"
+import fs from "fs/promises"
 
 async function main() {
-  const studies = [
-    "Portugal Municipal Energy",
-    "Lisbon Building Energy",
-    "FPO",
-  ].map((name, idx) => ({
-    name,
-    slug: name.toLowerCase().replaceAll(" ", "-"),
-    description: name,
-    imageSrc: `https://fakeimg.pl/600x400?text=${name}`,
-  }))
-  for (const study of studies) {
-    await prisma.study.upsert({
-      where: { slug: study.slug },
-      update: study,
-      create: study,
-    })
+  const files = await fs.readdir("./data", {})
+  for (const file of files) {
+    const data = yaml.load(file)
+    console.log(data)
+    const studies: Prisma.StudyCreateInput[] = [
+      "Portugal Municipal Energy",
+      "Lisbon Building Energy",
+      "FPO",
+    ].map((name, idx) => ({
+      name,
+      slug: name.toLowerCase().replaceAll(" ", "-"),
+      description: name,
+      imageSrc: `https://fakeimg.pl/600x400?text=${name}`,
+    }))
+
+    for (const study of studies) {
+      await prisma.study.upsert({
+        where: { slug: study.slug },
+        update: study,
+        create: study,
+      })
+    }
   }
 }
 
