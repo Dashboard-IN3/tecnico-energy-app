c2abab7ac468b258ddab37b6f80fef0611e818d5
Anthony Lukach <anthonylukach@gmail.com>
2024-02-05 17:51:14 -0800

    Rm unused models, study.href

--- components/card-grid.tsx
+++ components/card-grid.tsx
@@ -8,12 +8,12 @@ export default async function CardGrid() {
     <div className="container pr-4 md:pr-0 md:mx-auto py-8 md:max-w-max">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {studies &&
-          studies.studies?.map((study, key) => (
+          studies.studies?.map(study => (
             <Card
-              key={key}
+              key={study.id}
               description={study.description}
               title={study.title}
-              href={study.href}
+              href={`explore/${study.id}`}
               src={study.image_src}
             />
           ))}
