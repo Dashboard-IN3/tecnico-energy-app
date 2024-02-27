6c97794b7ad5bf7f45b6bc626f8680b62f4461b3
Emma Paz <emma@developmentseed.org>
2024-02-26 19:32:26 -0500

    update views

--- prisma/views/public/vw_map_fields.sql
+++ prisma/views/public/vw_map_fields.sql
@@ -1,26 +1,22 @@
 SELECT
   row_number() OVER () AS id,
   study_id,
-  theme_name,
-  theme_subtype,
-  scenario_name,
+  scenario_id,
+  scenario_subtype_id,
   field_name
 FROM
   (
     SELECT
-      metrics_metadata.study_id,
-      metrics_metadata.theme_name,
-      metrics_metadata.theme_subtype,
-      metrics_metadata.scenario_name,
-      metrics_metadata.field_name
+      mm.study_id,
+      mm.scenario_id,
+      mm.scenario_subtype_id,
+      mm.field_name
     FROM
-      metrics_metadata
+      metrics_metadata mm
     WHERE
-      (metrics_metadata.map_display = TRUE)
+      (mm.map_display = TRUE)
     ORDER BY
-      metrics_metadata.study_id,
-      metrics_metadata.theme_name,
-      metrics_metadata.scenario_name,
-      metrics_metadata.theme_subtype,
-      metrics_metadata.field_name
+      mm.study_id,
+      mm.scenario_id,
+      mm.field_name
   ) query;
