0f8bfeeb09f64d1a2a6b6d2a57687ce5c7ead7a0
Tammo Feldmann <31222040+Tammo-Feldmann@users.noreply.github.com>
2024-02-28 14:50:56 -0800

    Update example spec

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -1,18 +1,7 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test"
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+test("test", async ({ page }) => {
+  await page.goto("http://localhost:3000/")
+  await page.getByRole("link", { name: "Card Image Lisbon Building" }).click()
+  await expect(page.getByText("Urban Data Visualizer")).toBeVisible()
+})
