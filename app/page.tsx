6745860a499d541ad85a4c3f317334c25db9b258
Tammo Feldmann <31222040+Tammo-Feldmann@users.noreply.github.com>
2024-03-14 08:04:24 -0700

    Update card grid styles

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ export const revalidate = 3600 // revalidate the data at most every hour
 
 export default async function Home() {
   return (
-    <div className="bg-slate-100 h-full flex flex-col overflow-x-hidden">
+    <div className="bg-slate-100 h-full min-h-screen flex flex-col overflow-x-hidden">
       <Header />
-      <div className="container mx-2 px-2 sm:px-6 lg:px-8 py-8">
+      <div className="container mx-2 px-2 sm:px-6 lg:px-8 py-8 min-w-full">
         <h1 className="text-xl font-bold">Explore Urban Data Studies</h1>
         <CardGrid />
       </div>
--- components/card-grid.tsx
+++ components/card-grid.tsx
@@ -4,7 +4,7 @@ import { getStudies } from "@/app/lib/data"
 export default async function CardGrid() {
   const studies = await getStudies()
   return (
-    <div className="container pr-4 md:pr-0 md:mx-auto py-8 md:max-w-max">
+    <div className="container pr-4 md:pr-0 py-8 md:max-w-max min-w-full">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {studies?.map(study => (
           <Card
