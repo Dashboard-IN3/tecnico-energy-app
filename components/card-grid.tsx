a96b8aa417f3540b7fdd67254d6be8fb15ac3de0
Anthony Lukach <anthonylukach@gmail.com>
2024-03-20 10:52:56 -0700

    Simplify

--- components/card-grid.tsx
+++ components/card-grid.tsx
@@ -1,4 +1,3 @@
-import { study } from "@prisma/client"
 import Card from "./card"
 import { getStudies } from "@/app/lib/data"
 
@@ -8,7 +7,7 @@ export default async function CardGrid() {
   return (
     <div className="container pr-4 md:pr-0 md:mx-auto py-8 md:max-w-max">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {studies.map((study: study) => (
+        {studies.map(study => (
           <Card
             key={study.slug}
             description={study.description}
