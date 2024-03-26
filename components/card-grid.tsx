d60f662391499b5df10c1465dd9925674c85a9bc
Tammo Feldmann <31222040+Tammo-Feldmann@users.noreply.github.com>
2024-03-26 15:59:04 -0700

    Fix card grid styling

--- components/card-grid.tsx
+++ components/card-grid.tsx
@@ -5,9 +5,9 @@ export default async function CardGrid() {
   const studies = await getStudies()
 
   return (
-    <div className="container pr-4 md:pr-0 md:mx-auto py-8 md:max-w-max">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {studies.map(study => (
+    <div className="container pr-4 md:pr-0 py-8 md:max-w-max min-w-full">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols:6 gap-6">
+        {studies?.map(study => (
           <Card
             key={study.slug}
             description={study.description}
