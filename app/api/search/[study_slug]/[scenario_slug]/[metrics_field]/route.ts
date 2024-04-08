0abeddd33693b86410a180c503ed0588c769fc3b
Tammo Feldmann <31222040+Tammo-Feldmann@users.noreply.github.com>
2024-04-08 12:27:32 -0700

    Use preaggregated table for search

--- app/api/search/[study_slug]/[scenario_slug]/[metrics_field]/route.ts
+++ app/api/search/[study_slug]/[scenario_slug]/[metrics_field]/route.ts
@@ -9,28 +9,20 @@ export async function GET(req: NextRequest, { params }: { params: Params }) {
   // get all features if we have no aoi
   if (coordinates == "null") {
     const search = await prisma.$queryRaw`
-    WITH IntersectedGeometries AS (
-    SELECT
-        g.key AS id,
-        CAST(m.data -> ${metrics_field} ->> 'value' AS NUMERIC) AS data_value,
-        m.data -> ${metrics_field} ->> 'units' AS data_unit
-    FROM 
-        geometries g
-    JOIN  
-        scenario_metrics m ON g.key = m.geometry_key
-    WHERE
+      SELECT 
+        json_agg(g.key) as feature_ids, 
+        CAST(m.data -> ${metrics_field} ->> 'sum' AS NUMERIC) AS data_total,
+        CAST(m.data -> ${metrics_field} ->> 'avg' AS NUMERIC) AS data_avg,
+        CAST(m.data -> ${metrics_field} ->> 'units' AS VARCHAR) AS data_unit 
+      FROM 
+        geometries g, 
+        scenario_metrics_total m 
+      WHERE 
         (m.scenario_slug = ${scenario_slug} OR (m.scenario_slug IS NULL AND ${scenario_slug} = 'baseline'))
         AND
         g.study_slug = ${study_slug}
-      )
-      SELECT 
-          (SELECT SUM(data_value) FROM IntersectedGeometries) AS data_total,
-          (SELECT AVG(data_value) FROM IntersectedGeometries) AS data_avg,
-          (SELECT data_unit FROM IntersectedGeometries LIMIT 1) AS data_unit,
-          json_agg(id) AS feature_ids
-      FROM 
-          IntersectedGeometries
-  `
+      GROUP BY m.id
+    `
     return Response.json({ search })
   }
 
