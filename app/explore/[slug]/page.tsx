20183f026f316a81f4e7fc57fb3acf6bb834da82
Tammo Feldmann <31222040+Tammo-Feldmann@users.noreply.github.com>
2024-05-07 10:46:21 -0400

    Expande side pane width

--- app/explore/[slug]/page.tsx
+++ app/explore/[slug]/page.tsx
@@ -101,7 +101,7 @@ export default async function ExplorePage({
   return (
     <>
       <StoreInitialize stateObject={stateObject} />
-      <div className="grid grid-cols-1 grid-rows-[auto,1fr,1fr] md:grid-cols-[350px,1fr] md:grid-rows-[auto,1fr] h-screen w-full overflow-x-hidden">
+      <div className="grid grid-cols-1 grid-rows-[auto,1fr,1fr] md:grid-cols-[420px,1fr] md:grid-rows-[auto,1fr] h-screen w-full overflow-x-hidden">
         <div className="row-span-1 col-span-2 relative border-b border-gray-200">
           <Header />
         </div>
