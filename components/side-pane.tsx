a4abded3a2f3457ec5518c8026238257a20504a0
Tammo Feldmann <31222040+Tammo-Feldmann@users.noreply.github.com>
2024-05-09 10:56:33 -0400

    Hide theme dropdown if only one value

--- components/side-pane.tsx
+++ components/side-pane.tsx
@@ -94,22 +94,9 @@ export const SidePane: React.FC<Props> = ({ imgSrc, studyId }) => {
       <div className="w-full text-black text-xl font-extrabold">
         {selectedStudy.name}
       </div>
-      {/* {imgSrc && (
-        <div className="relative w-full min-h-[175px] max-w-[250px] md:max-w-lg">
-          <Image
-            className="w-full h-[200px] object-cover rounded-lg"
-            src={imgSrc}
-            fill={true}
-            alt="Placeholder"
-            sizes="(max-width: 150px) 100vw, (max-width: 150px) 50vw, 150px"
-            priority={true}
-          />
-        </div>
-      )} */}
 
       <div className="self-stretch justify-end items-start gap-6 inline-flex">
         <InPageLink href={`${studyId}/details`} label="Study Details" />
-        {/* <InPageLink href={`${studyId}/attributes`} label="Data Attribues" /> */}
         <div className="grow shrink basis-0 text-right text-black text-sm font-normal leading-tight">
           {selectedStudy.summary.totalSelectedFeatures} Features
         </div>
@@ -139,15 +126,17 @@ export const SidePane: React.FC<Props> = ({ imgSrc, studyId }) => {
           />
         </div>
       </div>
-      <DropdownMenu
-        title="Theme"
-        options={themeDropdownOptions}
-        selected={{
-          value: selectedTheme?.slug,
-          label: selectedTheme?.name,
-        }}
-        setSelected={option => setSelectedTheme(themes[option.value])}
-      />
+      {Object.values(themes).length > 1 && (
+        <DropdownMenu
+          title="Theme"
+          options={themeDropdownOptions}
+          selected={{
+            value: selectedTheme?.slug,
+            label: selectedTheme?.name,
+          }}
+          setSelected={option => setSelectedTheme(themes[option.value])}
+        />
+      )}
       <DropdownMenu
         title="Category"
         options={metricsOptions.categories}
