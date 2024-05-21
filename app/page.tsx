7040c8f658c68db4c18849b2ab288175ec7d5913
Tammo Feldmann <31222040+Tammo-Feldmann@users.noreply.github.com>
2024-05-21 13:51:07 -0400

    Update app/page.tsx
    Co-authored-by: Anthony Lukach <anthonylukach@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export const revalidate = 3600 // revalidate the data at most every hour
 
 export default async function Home() {
   return (
-    <PlausibleProvider domain="process.env.NEXT_PUBLIC_DOMAIN_NAME">
+    <PlausibleProvider domain={process.env.NEXT_PUBLIC_DOMAIN_NAME}>
       <div className="bg-slate-100 h-full min-h-screen flex flex-col overflow-x-hidden">
         <Header />
         <div className="container mx-2 px-2 sm:px-6 lg:px-8 py-8 min-w-full">
