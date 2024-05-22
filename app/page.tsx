1bea62ee721f895312133a080be0b4baec4d4fa5
Tammo Feldmann <31222040+Tammo-Feldmann@users.noreply.github.com>
2024-05-22 12:38:20 -0400

    Add plausible provider (#67)
    ## Summary
    
    This is the most basic addition of a plausible provider so we can
    collect analytics. I just used a pre-exiting package for nextJS. I
    already set up the listener on the plausible account. We just need to
    make sure that the environment variable for DOMAIN_NAME is available.
    
    ---------
    
    Co-authored-by: Anthony Lukach <anthonylukach@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
 import CardGrid from "@/components/card-grid"
 import { Header } from "@/components/header"
+import PlausibleProvider from "next-plausible"
 
 export const revalidate = 3600 // revalidate the data at most every hour
 
 export default async function Home() {
   return (
-    <div className="bg-slate-100 h-full min-h-screen flex flex-col overflow-x-hidden">
-      <Header />
-      <div className="container mx-2 px-2 sm:px-6 lg:px-8 py-8 min-w-full">
-        <h1 className="text-xl font-bold">Explore Urban Data Studies</h1>
-        <CardGrid />
+    <PlausibleProvider domain={process.env.NEXT_PUBLIC_DOMAIN_NAME || ""}>
+      <div className="bg-slate-100 h-full min-h-screen flex flex-col overflow-x-hidden">
+        <Header />
+        <div className="container mx-2 px-2 sm:px-6 lg:px-8 py-8 min-w-full">
+          <h1 className="text-xl font-bold">Explore Urban Data Studies</h1>
+          <CardGrid />
+        </div>
       </div>
-    </div>
+    </PlausibleProvider>
   )
 }
